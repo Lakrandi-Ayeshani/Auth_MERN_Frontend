9e73f4be0b9ceb9f76c089aa84e4e086b10a2b95
Lakrandi-Ayeshani <Lakrandiayeshani1204@gmail.com>
2023-09-15 01:58:07 +0530

    home page and user profile edit form developed

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import Hero from "../components/Hero.jsx"
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/task');
+    }
+  }, [navigate, userInfo]);
+
   return (
     <Hero />
   )
